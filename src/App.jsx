89bb398009aa3b3c7917d64b734d55bdf5bf4665
yosshyjungle <yosshyjungle@gmail.com>
2025-10-23 12:24:20 +0900

    link add

--- src/App.jsx
+++ src/App.jsx
@@ -60,11 +60,11 @@ function App() {
                         />
                       </svg>
                     </div>
-                    <h2 className='text-gray-900 text-lg font-medium ml-2'>SNS Chat App</h2>
+                    <h2 className='text-gray-900 text-lg font-medium ml-2'>Quiz App</h2>
                   </div>
                   <div>
-                    <p>ReactとJavaScriptで投稿機能つきのChatアプリ。仕様はReact, html, css, firebase, AWS,ログイン・ログアウト認証機能、メッセージ投稿機能、検索機能、お気に入り登録機能を実装しています。</p>
-                    <a href="#" className='mt-3 text-green-500 items-center'>もっと見る</a>
+                    <p>ReactとJavaScriptで簡単なクイズアプリ作成しました。デプロイはVercelでやっています。Reactの基本的な使い方がわかったので、成果物として作成しました。</p>
+                    <a href="https://quiz-app2-omega.vercel.app/" className='mt-3 text-green-500 items-center' target='_blank'>もっと見る</a>
                   </div>
                 </div>
               </div>
